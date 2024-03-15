8392b04198953f0e17afe10761e8bdfce4c30683
swamyvitasoft <swamy.vitasoft@gmail.com>
2024-03-15 15:01:51 +0530

    nirf code pushed

---
+++ application/views/nirf.php
@@ -0,0 +1,38 @@
+        <section class="inner-banner">
+            <div class="container">
+                <ul class="list-unstyled thm-breadcrumb">
+                    <li><a href="#">Home</a></li>
+                    <li class="active"><a href="#">NIRF</a></li>
+                </ul>
+                <h2 class="inner-banner__title">NIRF</h2>
+            </div>
+        </section>
+        <section class="about-one about-one__about-page">
+            <div class="container">
+                
+                
+                <div class="row">
+                   <div class="col-sm-5 col-lg-3">
+                      <ul class="nav nav-tabs flex-column mb-3">
+                         <li class="nav-item">
+                            <a class="nav-link active" data-toggle="tab" href="#tab-1">NIRF </a>
+                         </li>
+                      </ul>
+                   </div>
+                   <div class="col-sm-7 col-lg-9">
+                      <div class="row">
+                    <div class="col-lg-4">
+                        <div class="course-details__price h150" >
+                            <p class="course-details__price-text">NIRF 2024</p>
+                            <a href="uploads/nirf/Vageswari College of Engineering20240110-.pdf" target="_blank">(View/Download)</a>
+                        </div>
+                    </div>
+                </div>
+                       </div>
+                    </div>
+                
+                
+                
+                
+            </div>
+        </section>
